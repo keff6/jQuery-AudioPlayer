165ad050c59869bca8361b7200c7e52c89d1fd93
keff6 <kev.fallas@gmail.com>
2017-10-25 16:11:13 -0600

    Button Implementation and functionality

---
+++ js/main.js
@@ -0,0 +1,108 @@
+var audio;
+
+//Hide Pause Button
+$('#pause').hide();
+
+//Initialize
+initAudio($('#playlist li:first-child'));
+
+//Initializer funtion
+function initAudio(element){
+  var song = element.attr('song');
+  var title = element.text();
+  var cover = element.attr('cover');
+  var artist = element.attr('artist');
+  
+  //Create Audio Object
+  audio = new Audio('media/'+song);
+  
+  if(!audio.currentTime){
+    $('#duration').html('0:00');
+  }
+  
+  $('#audio-player .title').text(title);
+  $('#audio-player .artist').text(artist);
+  
+  //Insert Cover
+  $('img.cover').attr('src','./images/covers/'+cover);
+  
+  $('#playlist li').removeClass('active');
+  
+  element.addClass('active');
+  
+}
+
+//Play button
+$('#play').click(function(){
+  audio.play();
+  $('#play').hide();
+  $('#pause').show();
+  $('#duration').fadeIn(400);
+  showDuration();
+});
+
+//Pause button
+$('#pause').click(function(){
+  audio.pause();
+  $('#pause').hide();
+  $('#play').show();
+});
+
+//Stop Button
+$('#stop').click(function(){
+  audio.pause();
+  audio.currentTime = 0;
+  $('#pause').hide();
+  $('#play').show();
+  $('#duration').fadeOut(400);
+});
+
+//Next Button
+$('#next').click(function(){
+  audio.pause();
+  var next = $('#playlist li.active').next();
+  if(next.length == 0){
+    next = $('#playlist li:first-child');
+  }
+  initAudio(next);
+  audio.play();
+  showDuration();
+});
+
+//Next Button
+$('#prev').click(function(){
+  audio.pause();
+  var prev = $('#playlist li.active').prev();
+  if(prev.length == 0){
+    prev = $('#playlist li:last-child');
+  }
+  initAudio(prev);
+  audio.play();
+  showDuration();
+});
+
+
+//Volume Control
+$('#volume').change(function(){
+  audio.volume = parseFloat(this.value / 10);
+});
+
+//Time Duration
+function showDuration(){
+  $(audio).bind('timeupdate',function(){
+    //Get HOurs and minutes
+    var s = parseInt(audio.currentTime % 60);
+    var m = parseInt((audio.currentTime) / 60) % 60;
+    //Add 0 if less than 10
+    if(s < 10){
+       s = '0' + s;
+    }
+    $('#duration').html(m + ':' + s);
+    var value = 0;
+    //if is playing
+    if(audio.currentTime > 0){
+      value = Math.floor((100 / audio.duration) * audio.currentTime);
+    }
+    $('#progress').css('width', value+'%');
+  })
+}
